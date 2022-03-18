73179192aa18c9c39c994b56ffc38cea4473be77
Dru Mumba <drumumba@Drus-MacBook-Pro.local>
2022-03-18 14:06:30 +0300

    Hide crust size

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,14 @@
 
 
 /* UI Logic */
-/* $(document).ready(function () {
-	event.preventDefault
-}) */
+$(document).ready(function(){
+	$("form#pizza").submit(function (event) {
+		event.preventDefault();
+	})
+	$(".crust .crust-content").hide();
+	$(".size-select").click(function() {
+		var pizzaSize = $(this).attr("data-radio");
+		$(".crust .crust-content").hide();
+		$("." + pizzaSize).show();
+	});
+});
