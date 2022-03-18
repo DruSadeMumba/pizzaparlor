335b850f3274435e9b438c9ea3fcf26e2845445a
Dru Mumba <drumumba@Drus-MacBook-Pro.local>
2022-03-18 22:39:54 +0300

    Hide toppings

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,14 @@
 /* Business Logic */
+function validate() {
+	var pizzas = document.getElementsByClassName("pizza-select");
+	var sizes = document.getElementsByClassName("size-select");
+	var crusts = document.getElementsByClassName("crust-select");
+	var quantity = document.getElementsById("numb");
 
+	if (pizzas.value == "" || sizes.value == "" || crusts == "" || quantity == "");  {
+		alert("Please fill in required fields");
+	}
+}
 
 
 /* UI Logic */
@@ -12,5 +21,11 @@ $(document).ready(function(){
 		var pizzaSize = $(this).attr("data-radio");
 		$(".crust .crust-content").hide();
 		$("." + pizzaSize).show();
+	});
+	$(".toppings .toppings-content").hide();
+	$(".size-select").click(function() {
+		var toppingSize = $(this).attr("data-radio");
+		$(".toppings .toppings-content").hide();
+		$("." + toppingSize).show();
 	});
 });
