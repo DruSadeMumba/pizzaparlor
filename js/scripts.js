9a0089300050db52cad21dc268f691e8054c8e51
Dru Mumba <drumumba@Drus-MacBook-Pro.local>
2022-03-19 11:42:50 +0300

    Add toggle effect for pizzas and their ingredients

--- js/scripts.js
+++ js/scripts.js
@@ -13,6 +13,55 @@ function validate() {
 
 /* UI Logic */
 $(document).ready(function(){
+	$(".clickable1").click(function(){
+			$("#four-cheese-img").toggle();
+			$("#four-cheese-texts").toggle();
+	});
+
+	$(".clickable2").click(function(){
+		$("#veggie-img").toggle();
+		$("#veggie-texts").toggle();
+	});
+
+	$(".clickable3").click(function(){
+		$("#pepperoni-img").toggle();
+		$("#pepperoni-texts").toggle();
+	});
+
+	$(".clickable4").click(function(){
+		$("#meat-lovers-img").toggle();
+		$("#meat-lovers-texts").toggle();
+	});
+
+	$(".clickable5").click(function(){
+		$("#margherita-img").toggle();
+		$("#margherita-texts").toggle();
+	});
+
+	$(".clickable6").click(function(){
+		$("#hawaiian-img").toggle();
+		$("#hawaiian-texts").toggle();
+	});
+
+	$(".clickable7").click(function(){
+		$("#the-works-img").toggle();
+		$("#the-works-texts").toggle();
+	});
+
+	$(".clickable8").click(function(){
+		$("#bbq-chicken-img").toggle();
+		$("#bbq-chicken-texts").toggle();
+	});
+
+	$(".clickable9").click(function(){
+		$("#chicken-mushroom-img").toggle();
+		$("#chicken-mushroom-texts").toggle();
+	});
+});
+
+
+
+$(document).ready(function(){
 	$("form#pizza").submit(function (event) {
 		event.preventDefault();
 	})
